5fd53b2d655cb7b68b9882dbdcf7c4c57df56d8c
stena-he <stena-he@student.42wolfsburg.de>
2022-05-22 09:25:35 +0200

    add early ft_split.c

---
+++ libft/ft_split.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stena-he <stena-he@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/22 09:22:00 by stena-he          #+#    #+#             */
+/*   Updated: 2022/05/22 09:23:36 by stena-he         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/**
+ * @brief Allocates (with malloc(3)) and returns an array of strings 
+ * obtained by splitting ’s’ using the character ’c’ as a delimiter. The 
+ * array must end with a NULL pointer.
+ * 
+ * The array of new strings resulting from the split. NULL if the 
+ * allocation fails.
+ * 
+ * @param s 
+ * @param c 
+ * @return char** 
+ */
+char **ft_split(char const *s, char c)
+{
+	
+}
