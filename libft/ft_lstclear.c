933b4654170d18d88acb4742691b5623cdfb166a
Santiago Tena Hernandez <stenahe@gmail.com>
2022-05-27 20:38:23 +0200

    update

---
+++ libft/ft_lstclear.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stena-he <stena-he@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/27 19:14:46 by stena-he          #+#    #+#             */
+/*   Updated: 2022/05/27 20:33:44 by stena-he         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/**
+ * @brief Deletes and frees the given node and every successor of that node, 
+ * using the function ’del’ and free(3). 
+ * Finally, the pointer to the list must be set to NULL.
+ * 
+ * @param lst The address of a pointer to a node.
+ * @param del The address of the function used to delete the content of 
+ * the node.
+ */
+void ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*temporary;
+	t_list	*prev = NULL;
+	
+	temporary = *lst;	
+	while (temporary != NULL)
+	{		
+		del(temporary->content);
+		prev = temporary;
+		temporary = temporary->next;
+		free(prev);	
+	}
+	*lst = NULL;
+}
--- libft/ft_lstdelone.c
+++ libft/ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: stena-he <stena-he@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/27 13:53:43 by stena-he          #+#    #+#             */
-/*   Updated: 2022/05/27 19:11:14 by stena-he         ###   ########.fr       */
+/*   Updated: 2022/05/27 20:34:58 by stena-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,6 @@
  */
 void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
-	t_list	*temporary;
-
-	temporary = lst;	
-	while (temporary != NULL)
-	{
-		if (temporary->content == &lst)
-			del(&temporary);
-		temporary = temporary->next;
-	}
+	del(lst->content);
+	free(lst);
 }
