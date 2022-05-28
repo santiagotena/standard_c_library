e65cbd0d90f1f4281103c7e55a138657593b2281
Santiago Tena Hernandez <stenahe@gmail.com>
2022-05-28 15:43:57 +0200

    update libft.h

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,11 +6,23 @@
 /*   By: stena-he <stena-he@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/28 11:56:32 by stena-he          #+#    #+#             */
-/*   Updated: 2022/05/28 12:09:09 by stena-he         ###   ########.fr       */
+/*   Updated: 2022/05/28 15:34:49 by stena-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*output;
+
+	output = malloc(sizeof(t_list));
+	if (!output)
+		return (NULL);
+	output->content = content;
+	output->next = NULL;
+	return (output);
+}
 
 /**
  * @brief Iterates the list ’lst’ and applies the function ’f’ on the 
@@ -29,11 +41,18 @@
 t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*temporary;
+	t_list	*new;
 
 	temporary = lst;
 	while (temporary != NULL)
 	{
-		f(temporary->content);
+		new = ft_lstnew(f(temporary->content));
+		if (!new)
+		{
+			del(new->content);
+			return (NULL);
+		}
 		temporary = temporary->next;
 	}
+	return (new);
 }
