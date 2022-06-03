6b0acec8db59154c286d314bcde411a804a53384
Santiago Tena Hernandez <stenahe@gmail.com>
2022-06-04 01:20:34 +0200

    update for edge case

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: stena-he <stena-he@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/16 15:04:54 by stena-he          #+#    #+#             */
-/*   Updated: 2022/06/04 00:18:38 by stena-he         ###   ########.fr       */
+/*   Updated: 2022/06/04 01:19:30 by stena-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ static size_t	ft_forward(size_t s1_index, char *output, \
 	return (s1_index);
 }
 
-static size_t	ft_reverse(size_t rev_s1_index, size_t set_index,\
+static size_t	ft_reverse(size_t rev_s1_index, size_t set_index, \
 	char const *set, char *output)
 {
 	int		match;
@@ -55,6 +55,11 @@ static size_t	ft_reverse(size_t rev_s1_index, size_t set_index,\
 			set_index++;
 		}
 		set_index = 0;
+		if (rev_s1_index == (size_t) 1)
+		{
+			rev_s1_index = 0;
+			break ;
+		}
 		if (match == 0)
 			break ;
 		match = 0;
@@ -84,89 +89,92 @@ char	*ft_strtrim(char const *s1, char const *set)
 	set_index = 0;
 	s1_index = ft_forward(s1_index, output, set_index, set);
 	rev_s1_index = ft_reverse(rev_s1_index, set_index, set, output);
-	output = ft_substr(s1, s1_index, rev_s1_index - s1_index + 1);
+	if (s1_index > rev_s1_index)
+		output = ft_substr(s1, s1_index, s1_index - rev_s1_index + 1);
+	else
+		output = ft_substr(s1, s1_index, rev_s1_index - s1_index + 1);
 	if (!output)
 		return (NULL);
 	return (output);
 }
 
-//Debugger
+// //Debugger
 
-size_t	ft_strlen(const char *str)
-{
-	size_t		counter;
+// size_t	ft_strlen(const char *str)
+// {
+// 	size_t		counter;
 
-	counter = 0;
-	while (str[counter] != '\0')
-	{
-		counter += 1;
-	}
-	return (counter);
-}
+// 	counter = 0;
+// 	while (str[counter] != '\0')
+// 	{
+// 		counter += 1;
+// 	}
+// 	return (counter);
+// }
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
-{
-	char	*output;
-	size_t	substr_len;
-	size_t	output_index;
+// char	*ft_substr(char const *s, unsigned int start, size_t len)
+// {
+// 	char	*output;
+// 	size_t	substr_len;
+// 	size_t	output_index;
 
-	if (s == NULL)
-		return (NULL);
-	substr_len = ft_strlen(s) - start;
-	if (start >= ft_strlen(s))
-		return (ft_strdup(""));
-	if ((size_t) ft_strlen(s) < len)
-		len = ft_strlen(s);
-	output = (char *)malloc(substr_len * sizeof(char));
-	if (!output)
-		return (NULL);
-	output_index = 0;
-	while (output_index < len)
-	{
-		output[output_index] = s[start];
-		output_index++;
-		start++;
-	}
-	output[output_index] = '\0';
-	return (output);
-}
+// 	if (s == NULL)
+// 		return (NULL);
+// 	substr_len = ft_strlen(s) - start;
+// 	if (start >= ft_strlen(s))
+// 		return (ft_strdup(""));
+// 	if ((size_t) ft_strlen(s) < len)
+// 		len = ft_strlen(s);
+// 	output = (char *)malloc(substr_len * sizeof(char));
+// 	if (!output)
+// 		return (NULL);
+// 	output_index = 0;
+// 	while (output_index < len)
+// 	{
+// 		output[output_index] = s[start];
+// 		output_index++;
+// 		start++;
+// 	}
+// 	output[output_index] = '\0';
+// 	return (output);
+// }
 
-char	*ft_strdup(const char *s1)
-{
-	size_t	count;
-	char	*output;
-	size_t	index;
+// char	*ft_strdup(const char *s1)
+// {
+// 	size_t	count;
+// 	char	*output;
+// 	size_t	index;
 
-	index = 0;
-	count = 0;
-	while (s1[index] != '\0')
-	{
-		count++;
-		index++;
-	}
-	output = (char *)malloc((count + 1) * sizeof(char));
-	if (!output)
-		return (NULL);
-	index = 0;
-	while (index < count)
-	{
-		output[index] = s1[index];
-		index++;
-	}
-	output[index] = '\0';
-	return (output);
-}
+// 	index = 0;
+// 	count = 0;
+// 	while (s1[index] != '\0')
+// 	{
+// 		count++;
+// 		index++;
+// 	}
+// 	output = (char *)malloc((count + 1) * sizeof(char));
+// 	if (!output)
+// 		return (NULL);
+// 	index = 0;
+// 	while (index < count)
+// 	{
+// 		output[index] = s1[index];
+// 		index++;
+// 	}
+// 	output[index] = '\0';
+// 	return (output);
+// }
 
-# include <ctype.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <string.h>
-# include <unistd.h>
+// # include <ctype.h>
+// # include <stdio.h>
+// # include <stdlib.h>
+// # include <string.h>
+// # include <unistd.h>
 
-int main(void)
-{
-	char s1[] = " lorem ipsum dolor sit amet";
-	printf("%s\n", s1);
-	printf("%s\n", ft_strtrim(s1, "l "));
-	return (0);
-}
+// int main(void)
+// {
+// 	char s1[] = "          ";
+// 	printf("%s\n", s1);
+// 	printf("%s\n", ft_strtrim(s1, " "));
+// 	return (0);
+// }
