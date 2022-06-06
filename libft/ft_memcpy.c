e9e1ca69c4b8639fef7d03c742789250330f5114
Santiago Tena Hernandez <stenahe@gmail.com>
2022-06-07 00:10:21 +0200

    add protection

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: stena-he <stena-he@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/28 17:08:40 by stena-he          #+#    #+#             */
-/*   Updated: 2022/05/18 17:47:15 by stena-he         ###   ########.fr       */
+/*   Updated: 2022/06/06 22:44:47 by stena-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,8 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	counter = 0;
 	ptr_dst = (char *)dst;
 	ptr_src = (char *)src;
+	if (dst == NULL && src == NULL)
+		return (0);
 	while (counter < n)
 	{
 		ptr_dst[counter] = ptr_src[counter];
